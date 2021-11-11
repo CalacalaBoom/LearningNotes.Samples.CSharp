77e2963c403287512ca5e04c2e44971996bb82ac
CalacalaBoom <zhangyu19990502@163.com>
2021-11-11 22:21:54 +0800

    更新了一维数组和二维数组基本概念的实例

---
+++ OneDimensionalArray/OneDimensionalArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] day = new int[] { 31,28,31,30,31,30,31,31,30,31,30,31};
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine("{0}月有{1}天",i+1,day[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoDimensionalArray/TwoDimensionalArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] arr = new string[9][];
+            for (int n=0;n<9;n++)
+            {
+                arr[n] = new string[n+1];
+                for (int i = 0; i < arr[n].Length; i++)
+                {
+                    arr[n][i] = (i + 1) + "×" + (n + 1) + "=" + (n + 1) * (i + 1);
+                    Console.Write(arr[n][i]+"  ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
