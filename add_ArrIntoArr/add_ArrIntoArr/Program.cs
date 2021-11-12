63c218484f154f7e6ad9874b85a90b445505ec5d
CalacalaBoom <zhangyu19990502@163.com>
2021-11-12 22:34:54 +0800

    更新了添加数组元素的实例

---
+++ add_ArrIntoArr/add_ArrIntoArr/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_ArrIntoArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {0,1,2,3,8,9};
+            int[] addarr = new int[] {4,5,6,7};
+            Console.WriteLine("插入之前的数组元素：");
+            foreach (int n in arr)
+            {
+                Console.Write(n+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("要插入的数组元素：");
+            foreach (int n in addarr)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+            int[] arrtem = Addarr(arr,addarr,3);
+            Console.WriteLine("插入之后的数组元素：");
+            foreach (int n in arrtem)
+            {
+                Console.Write(n + " ");
+            }
+            Console.ReadLine();
+        }
+        static int[] Addarr(int[] arr,int[] addarr,int index)
+        {
+            int[] arrtem = new int[arr.Length+addarr.Length];
+            if (index >= (arr.Length))
+            {
+                index = arr.Length - 1;
+            }
+            for (int i = 0; i < arrtem.Length; i++)
+            {
+                if (index >= 0)
+                {
+                    if (i < (index + 1))
+                    {
+                        arrtem[i] = arr[i];
+                    }
+                    else if (i == (index + 1))
+                    {
+                        for (int j = 0; j < addarr.Length; j++)
+                        {
+                            arrtem[i + j] = addarr[j];
+                        }
+                        i = i + (addarr.Length - 1);
+                    }
+                    else
+                    {
+                        arrtem[i] = arr[i - addarr.Length];
+                    }
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        for (int j = 0; j < arr.Length; j++)
+                        {
+                            arrtem[i + j] = addarr[j];
+                        }
+                        i = i + (addarr.Length - 1);
+                    }
+                    else
+                    {
+                        arrtem[i] = arr[i - addarr.Length];
+                    }
+                }
+            }
+            return arrtem;
+        }
+    }
+}
---
+++ add_ElementIntoArr/add_ElementIntoArr/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_ElementIntoArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {0,1,2,3,4,6,7,8,9};
+            Console.WriteLine("原数组元素");
+            foreach (int n in arr)
+            {
+                Console.Write(n+" ");
+            }
+            Console.WriteLine();
+            arr=AddArr(arr,4,5);
+            Console.WriteLine("插入元素之后的数组元素：");
+            foreach (int n in arr)
+            {
+                Console.Write(n+" ");
+            }
+            Console.ReadLine();
+            
+        }
+        static int[] AddArr(int[] arrB,int index,int value)
+        {
+            if (index >= arrB.Length)
+            {
+                index = arrB.Length - 1;
+            }
+            int[] arrTem = new int[arrB.Length+1];
+            for (int i = 0; i < arrTem.Length; i++)
+            {
+                if (index > 0)
+                {
+                    if (i < (index + 1))
+                    {
+                        arrTem[i] = arrB[i];
+                    }
+                    else if (i == index + 1)
+                    {
+                        arrTem[i] = value;
+                    }
+                    else
+                    {
+                        arrTem[i] = arrB[i - 1];
+                    }
+                }
+                else if (index == 0)
+                {
+                    if (i == 0)
+                    {
+                        arrTem[i] = value;
+                    }
+                    else
+                    {
+                        arrTem[i] = arrB[i - 1];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("插入位置错误");
+                }
+            }
+
+            return arrTem;
+        }
+    }
+}
