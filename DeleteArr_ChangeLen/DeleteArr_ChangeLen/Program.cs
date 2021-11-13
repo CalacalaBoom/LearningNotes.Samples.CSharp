2ad67b9fbcd6ae3ae1bb2598b254a6a9350d1190
CalacalaBoom <zhangyu19990502@163.com>
2021-11-13 21:00:19 +0800

    更新了以是否改变数组长度为区别的两种删除数组中元素的方法

---
+++ DeleteArr_ChangeLen/DeleteArr_ChangeLen/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteArr_ChangeLen
+{
+    class Program
+    {
+        static string[] DeleteArr(string[] arr, int index, int Len)
+        {
+            if (Len <= 0)
+            {
+                return arr;
+            }
+            if (index == 0 && Len >= arr.Length)
+            {
+                Len = arr.Length;
+            }
+            else if ((index+Len)>=arr.Length)
+            {
+                Len = arr.Length;
+            }
+            string[] temarr = new string[arr.Length-Len];
+            for (int i = 0; i < temarr.Length; i++)
+            {
+                if (i >= index)
+                {
+                    temarr[i] = arr[i + Len];
+                }
+                else
+                {
+                    temarr[i] = arr[i];
+                }
+            }
+            return temarr;
+        }
+        static void Main(string[] args)
+        {
+            String[] arr = new string[] {"i","am","seting","here","in","a","boring","room"};
+            Console.Write("原数组：");
+            foreach (string n in arr)
+            {
+                Console.Write(n+" ");
+            }
+            Console.WriteLine();
+            String[] arrTem = DeleteArr(arr,0,1);
+            Console.Write("删除后的数组：");
+            foreach (string n in arrTem)
+            {
+                Console.Write(n + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ deleteArr/deleteArr/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deleteArr
+{
+    class Program
+    {
+        static void DeleteAArr(String[] arr,int index,int Len)
+        {
+            if (Len < 0)
+            {
+                return;
+            }
+            if (index == 0 && Len >= arr.Length)
+            {
+                Len = arr.Length;
+            }
+            else if ((index+Len)>=arr.Length)
+            {
+                Len = arr.Length - index - 1;
+            }
+            for (int i = 0; i < arr.Length - index - Len; i++)
+            {
+                arr[i + index] = arr[index+Len+i];
+            }
+            for (int j = (arr.Length - 1); j > (arr.Length - Len - 1); j--)
+            {
+                arr[j] = null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            String[] arr = new string[] {"dog","cat","pig","bird","duck","flower"};
+            Console.Write("原数组：");
+            foreach (string n in arr)
+            {
+                Console.Write(n+" ");
+            }
+            Console.WriteLine();
+            DeleteAArr(arr,0,1);
+            Console.Write("删除后的数组：");
+            foreach (string n in arr)
+            {
+                Console.Write(n + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
